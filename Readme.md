d5fd7d0d137f3d42277b3f8cd88c52c279e96ad1
Sarthak Sharma <sarthak753@gmail.com>
2024-02-23 15:51:21 +0530

    UserSigningUp

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Make sure you have installed Node pacakge required: { node: '^18.0.0 || >=20.0.0' }
+In the frontend folder: "npm create vite@latest ."
+In the root folder: "npm init -y"
+Install packages: express dotenv cookie-parser bcryptjs mongoose socket.io jsonwebtoken nodemon
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+// const express = require("express")   // To use express by importing, you need to put ""type": "module"" in package.json
+import dotenv from "dotenv";
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+
+const app = express();
+const PORT= process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());  // to parse json
+app.use("/api/auth",authRoutes);    //Middlewares
+
+
+// app.get("/", (req,res)=>{
+//     //root route https:localhost:5000/
+//     res.send("Hello World!!!!");
+// });
+
+app.listen (PORT,() => {
+    connectToMongoDB() ;
+    console.log('Server is running on port ${PORT}');
+});
+   
